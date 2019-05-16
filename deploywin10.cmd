04da3466225b8095bca67fd44470751df5964f8e
Valentin Illich <Valentin.Illich@rohde-schwarz.com>
2019-05-16 13:21:02 +0200

    using two arguments now for source path of dlls and destination directory

--- deploywin10.cmd
+++ deploywin10.cmd
@@ -1,19 +1,19 @@
 REM debug versions
 
-xcopy /D /Y C:\Qt\5.12.2\mingw73_32\bin\Qt5Cored.dll %1\debug
-xcopy /D /Y C:\Qt\5.12.2\mingw73_32\bin\Qt5Widgetsd.dll %1\debug
-xcopy /D /Y C:\Qt\5.12.2\mingw73_32\bin\Qt5Guid.dll %1\debug
+xcopy /D /Y %1\bin\Qt5Cored.dll %2\debug
+xcopy /D /Y %1\bin\Qt5Widgetsd.dll %2\debug
+xcopy /D /Y %1\bin\Qt5Guid.dll %2\debug
 
-xcopy /D /Y C:\Qt\Tools\mingw730_32\bin\libgcc_s_dw2-1.dll %1\debug
-xcopy /D /Y C:\Qt\Tools\mingw730_32\bin\libwinpthread-1.dll %1\debug
-xcopy /D /Y C:\Qt\Tools\mingw730_32\bin\libstdc++-6.dll %1\debug
+xcopy /D /Y %1\bin\libgcc_s_dw2-1.dll %2\debug
+xcopy /D /Y %1\bin\libwinpthread-1.dll %2\debug
+xcopy /D /Y %1\bin\libstdc++-6.dll %2\debug
 
 REM relese versions
 
-xcopy /D /Y C:\Qt\5.12.2\mingw73_32\bin\Qt5Core.dll %1\release
-xcopy /D /Y C:\Qt\5.12.2\mingw73_32\bin\Qt5Widgets.dll %1\release
-xcopy /D /Y C:\Qt\5.12.2\mingw73_32\bin\Qt5Gui.dll %1\release
+xcopy /D /Y %1\bin\Qt5Core.dll %2\release
+xcopy /D /Y %1\bin\Qt5Widgets.dll %2\release
+xcopy /D /Y %1\bin\Qt5Gui.dll %2\release
 
-xcopy /D /Y C:\Qt\Tools\mingw730_32\bin\libgcc_s_dw2-1.dll %1\release
-xcopy /D /Y C:\Qt\Tools\mingw730_32\bin\libwinpthread-1.dll %1\release
-xcopy /D /Y C:\Qt\Tools\mingw730_32\bin\libstdc++-6.dll %1\release
+xcopy /D /Y %1\bin\libgcc_s_dw2-1.dll %2\release
+xcopy /D /Y %1\bin\libwinpthread-1.dll %2\release
+xcopy /D /Y %1\bin\libstdc++-6.dll %2\release
