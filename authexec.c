c84ce240f6968a1795ac9c46a6b4069ba8f65aab
Valentin Illich <Valentin.Illich@rohde-schwarz.com>
2019-05-16 13:22:26 +0200

    dealing with -Wunused-parameter gcc warning

--- authexec.c
+++ authexec.c
@@ -11,41 +11,40 @@
 #include <stdio.h>
 #endif
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
 #if defined(Q_OS_WIN)
 void checkForPasswdHelper(int argc, char **argv)
 {
-    printf("%d,%s",argc,argv[0]);
-}
-#endif
 #if defined(Q_OS_MAC)
-void checkForPasswdHelper(int argc, char **argv)
-{
-  if( strstr(argv[0],"pwdhelper")!=0 )
-  {
-    static char buff[128];
-    FILE *fp=fopen("/tmp/passwd","r");
-    if( fp )
+    if( strstr(argv[0],"pwdhelper")!=0 )
     {
-      size_t read = fread(buff,1,127,fp);
-      buff[read] = 0x0;
-      fclose(fp);
-      remove("/tmp/passwd");
+      static char buff[128];
+      FILE *fp=fopen("/tmp/passwd","r");
+      if( fp )
+      {
+        size_t read = fread(buff,1,127,fp);
+        buff[read] = 0x0;
+        fclose(fp);
+        remove("/tmp/passwd");
+      }
+      fprintf(stdout,"%s",buff);
+      exit(0);
     }
-    fprintf(stdout,"%s",buff);
-    exit(0);
-  }
+#endif
 }
 #endif
 
-int getAdminRights(int argc, char* argv[],char *password) {
+int getAdminRights(int argc, char* argv[], char *password)
+{
     int result = -1;
 
     if( argc==2 && strcmp(argv[1],"-admin")==0 )
         return 0;
     else if( argc==1 )
     {
-#if defined(Q_OS_WIN32)
-        printf("%s",password);
+#if defined(Q_OS_WIN)
         HANDLE child = ShellExecuteA(NULL, "runas", argv[0], "-admin", NULL, SW_SHOWNORMAL);
         if (child) {
           // User accepted UAC prompt (gave permission).
@@ -78,3 +77,5 @@ int getAdminRights(int argc, char* argv[],char *password) {
 
     return result;
 }
+
+#pragma GCC diagnostic pop
