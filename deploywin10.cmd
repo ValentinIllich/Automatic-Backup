40eefe77504bc019904720400204e723adac3cda
Valentin Illich <Valentin.Illich@rohde-schwarz.com>
2019-10-08 10:41:02 +0200

    changed signature because of different source paths: arg1 - root path for QT dlls (ending before /bin) arg2 - root path for mingw system dlls (ending before /bin) arg3 - destination

--- deploywin10.cmd
+++ deploywin10.cmd
@@ -1,19 +1,19 @@
 REM debug versions
 
-xcopy /D /Y %1\bin\Qt5Cored.dll %2\debug
-xcopy /D /Y %1\bin\Qt5Widgetsd.dll %2\debug
-xcopy /D /Y %1\bin\Qt5Guid.dll %2\debug
+xcopy /D /Y %1\bin\Qt5Cored.dll %3\debug
+xcopy /D /Y %1\bin\Qt5Widgetsd.dll %3\debug
+xcopy /D /Y %1\bin\Qt5Guid.dll %3\debug
 
-xcopy /D /Y %1\bin\libgcc_s_dw2-1.dll %2\debug
-xcopy /D /Y %1\bin\libwinpthread-1.dll %2\debug
-xcopy /D /Y %1\bin\libstdc++-6.dll %2\debug
+xcopy /D /Y %2\bin\libgcc_s_dw2-1.dll %3\debug
+xcopy /D /Y %2\bin\libwinpthread-1.dll %3\debug
+xcopy /D /Y %2\bin\libstdc++-6.dll %3\debug
 
 REM relese versions
 
-xcopy /D /Y %1\bin\Qt5Core.dll %2\release
-xcopy /D /Y %1\bin\Qt5Widgets.dll %2\release
-xcopy /D /Y %1\bin\Qt5Gui.dll %2\release
+xcopy /D /Y %1\bin\Qt5Core.dll %3\release
+xcopy /D /Y %1\bin\Qt5Widgets.dll %3\release
+xcopy /D /Y %1\bin\Qt5Gui.dll %3\release
 
-xcopy /D /Y %1\bin\libgcc_s_dw2-1.dll %2\release
-xcopy /D /Y %1\bin\libwinpthread-1.dll %2\release
-xcopy /D /Y %1\bin\libstdc++-6.dll %2\release
+xcopy /D /Y %2\bin\libgcc_s_dw2-1.dll %3\release
+xcopy /D /Y %2\bin\libwinpthread-1.dll %3\release
+xcopy /D /Y %2\bin\libstdc++-6.dll %3\release
