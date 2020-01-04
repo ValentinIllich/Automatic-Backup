f8ddfc1a310ffa802184fe3f6697ee6bc06b5b7e
ValentinIllich <Valentin.Illich@web.de>
2020-01-04 12:39:20 +0100

    mingw 7.3

--- deploywin10.cmd
+++ deploywin10.cmd
@@ -4,7 +4,7 @@ xcopy /D /Y %1\bin\Qt5Cored.dll %3\debug
 xcopy /D /Y %1\bin\Qt5Widgetsd.dll %3\debug
 xcopy /D /Y %1\bin\Qt5Guid.dll %3\debug
 
-xcopy /D /Y %2\bin\libgcc_s_dw2-1.dll %3\debug
+xcopy /D /Y %2\bin\libgcc_s_seh-1.dll %3\debug
 xcopy /D /Y %2\bin\libwinpthread-1.dll %3\debug
 xcopy /D /Y %2\bin\libstdc++-6.dll %3\debug
 
@@ -14,6 +14,6 @@ xcopy /D /Y %1\bin\Qt5Core.dll %3\release
 xcopy /D /Y %1\bin\Qt5Widgets.dll %3\release
 xcopy /D /Y %1\bin\Qt5Gui.dll %3\release
 
-xcopy /D /Y %2\bin\libgcc_s_dw2-1.dll %3\release
+xcopy /D /Y %2\bin\libgcc_s_seh-1.dll %3\release
 xcopy /D /Y %2\bin\libwinpthread-1.dll %3\release
 xcopy /D /Y %2\bin\libstdc++-6.dll %3\release
